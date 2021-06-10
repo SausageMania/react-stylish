f08b083efa5fe8399bc8a68ccea99df04283a779
minsik.um@vatech.com <minsik.um@vatech.com>
2021-06-10 17:51:05 +0900

    fix yarn start error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { ThemeProvider } from 'react-jss';
-import theme from 'styles/theme';
+import React, {useState} from 'react';
+import { ThemeProvider, createUseStyles} from 'react-jss';
+import theme from './styles/theme';
+import { CCButton } from './components/CCButton';
+
+const styles = createUseStyles({
+  container: {
+    paddingLeft: "15px",
+    paddingTop: "15px",
+    display: "flex",
+  },
+  buttonContainer: {
+    marginRight: "5px"
+  },
+  text: {
+    padding: "15px",
+  }
+})
 
 const App = () => {
+  const classes = styles();
+  const [text, setText] = useState({
+    content: null,
+    count: 0,
+  });
+
+  const onClickHandle = () => {
+    setText({content:'You clicked', count: text.count + 1});
+  }
+
+  const resetHandle = () => {
+    setText({content: null, count: 0});
+  }
+
   return (
-  <ThemeProvider theme={theme}>
-    <div></div>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <div className={classes.container}>
+        <div className={classes.buttonContainer}>
+          <CCButton variant="contained" onClick={onClickHandle}>Button</CCButton>
+        </div>
+        <div>
+        <CCButton variant="contained" color="error" onClick={resetHandle}>Reset</CCButton>
+        </div>
+      </div>
+      <div className={classes.text}>
+        {text.content && <p>{text.content} {text.count} times!</p>}
+      </div>
+    </ThemeProvider>
   );
 }
 
