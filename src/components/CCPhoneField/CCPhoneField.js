2a196c1fd4376dcc8a2a8ac51f8ea465fbd90410
vatech-minsik <minsik.um@vatech.com>
2021-11-11 16:05:38 +0900

    Fix the position of CCSelect

--- src/components/CCPhoneField/CCPhoneField.js
+++ src/components/CCPhoneField/CCPhoneField.js
@@ -1,35 +1,35 @@
-import React, { forwardRef, useState } from "react";
-import { createUseStyles, useTheme } from "react-jss";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
+/* eslint-disable no-unused-vars */
+import React, { forwardRef, useState } from 'react';
+import { createUseStyles, useTheme } from 'react-jss';
+// import PhoneInput from "react-phone-input-2";
+// import "react-phone-input-2/lib/style.css";
 
-const useStyles = createUseStyles((theme) => ({
+const useStyles = createUseStyles(theme => ({
   phone__field: {
-    width: (props) => props.fullWidth && "100%",
-    height: (props) => (props.height ? props.height : "36px"),
+    width: props => props.fullWidth && '100%',
+    height: props => (props.height ? props.height : '36px'),
   },
 }));
 
 const CCPhoneField = forwardRef((props, ref) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const { fullWidth, height, ...others } = props;
 
-  // eslint-disable-next-line no-unused-vars
   const classes = useStyles(props);
-  // eslint-disable-next-line no-unused-vars
   const theme = useTheme();
 
   return (
-    <PhoneInput
-      value={value}
-      onChange={(phone) => setValue(phone)}
-      inputStyle={{
-        width: fullWidth && "100%",
-        height: height ? height : "36px",
-        boxshadow: "0 0 0 0 red",
-      }}
-      {...others}
-    />
+    // <PhoneInput
+    //   value={value}
+    //   onChange={(phone) => setValue(phone)}
+    //   inputStyle={{
+    //     width: fullWidth && "100%",
+    //     height: height ? height : "36px",
+    //     boxshadow: "0 0 0 0 red",
+    //   }}
+    //   {...others}
+    // />
+    <div>Currently making the component.</div>
   );
 });
 
