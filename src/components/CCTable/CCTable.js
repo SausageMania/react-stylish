4a25a34de44ad6afdd1eb8ad69bbab99ac8cdb43
vatech-minsik <minsik.um@vatech.com>
2021-11-24 16:35:18 +0900

    Add row.className to customize row easily

--- src/components/CCTable/CCTable.js
+++ src/components/CCTable/CCTable.js
@@ -1,54 +1,48 @@
-import React, {
-  forwardRef,
-  useState,
-  useRef,
-  useEffect,
-  useCallback,
-} from "react";
-import { createUseStyles } from "react-jss";
-import clsx from "clsx";
-import PropTypes from "prop-types";
+import React, { forwardRef, useState, useRef, useEffect, useCallback } from 'react';
+import { createUseStyles } from 'react-jss';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
 
-const useStyles = createUseStyles((theme) => ({
+const useStyles = createUseStyles(theme => ({
   table: {
-    width: (props) => (props.width ? props.width : "none"),
-    maxHeight: (props) => (props.height ? props.height : "none"),
-    overflow: "auto",
-    position: "relative",
-    fontSize: (props) => (props.fontSize ? props.fontSize : "none"),
+    width: props => (props.width ? props.width : 'none'),
+    maxHeight: props => (props.height ? props.height : 'none'),
+    overflow: 'auto',
+    position: 'relative',
+    fontSize: props => (props.fontSize ? props.fontSize : 'none'),
   },
   table__column: {
-    width: (props) => {
+    width: props => {
       let totalWidth = 0;
-      props.columns.forEach((column) => {
+      props.columns.forEach(column => {
         totalWidth += column.width ? column.width : 200;
         totalWidth += 10; // 각 table당 margin값을 더해준다.
       });
       return totalWidth;
     },
-    display: "flex",
-    position: (props) => (props.disableSticky ? "static" : "sticky"),
+    display: 'flex',
+    position: props => (props.disableSticky ? 'static' : 'sticky'),
     top: 0,
-    alignItems: "center",
+    alignItems: 'center',
     borderBottom: `1px solid ${theme.palette.border.main}`,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF',
     zIndex: 1,
   },
   column__content: {
     // overflow: "hidden",
     // textOverflow: "ellipsis",
     // whiteSpace: "nowrap",
-    wordBreak: "break-word",
-    margin: "0 5px",
-    display: "flex",
-    justifyContent: (props) => (props.justify ? props.justify : "flex-start"),
-    alignItems: "center",
-    minHeight: "35px",
+    wordBreak: 'break-word',
+    margin: '0 5px',
+    display: 'flex',
+    justifyContent: props => (props.justify ? props.justify : 'flex-start'),
+    alignItems: 'center',
+    minHeight: '35px',
   },
   table__row: {
-    width: (props) => {
+    width: props => {
       let totalWidth = 0;
-      props.columns.forEach((column) => {
+      props.columns.forEach(column => {
         totalWidth += column.width ? column.width : 200;
         // totalWidth += 10; // 각 table당 margin값을 더해준다.
       });
@@ -56,12 +50,11 @@ const useStyles = createUseStyles((theme) => ({
     },
   },
   row__body: {
-    display: "flex",
-    alignItems: "center",
-    cursor: (props) => (props.disableSelect ? "default" : "pointer"),
-    "&:hover": {
-      backgroundColor: (props) =>
-        !props.disableHover && theme.palette.result.main,
+    display: 'flex',
+    alignItems: 'center',
+    cursor: props => (props.disableSelect ? 'default' : 'pointer'),
+    '&:hover': {
+      backgroundColor: props => !props.disableHover && theme.palette.result.main,
     },
   },
   row__border: {
@@ -74,15 +67,15 @@ const useStyles = createUseStyles((theme) => ({
     // overflow: "hidden",
     // textOverflow: "ellipsis",
     // whiteSpace: "nowrap",
-    wordBreak: "break-word",
-    margin: "0 5px",
-    minHeight: "35px",
-    display: "flex",
-    justifyContent: (props) => (props.justify ? props.justify : "flex-start"),
-    alignItems: "center",
+    wordBreak: 'break-word',
+    margin: '0 5px',
+    minHeight: '35px',
+    display: 'flex',
+    justifyContent: props => (props.justify ? props.justify : 'flex-start'),
+    alignItems: 'center',
   },
   checkbox: {
-    width: "50px",
+    width: '50px',
   },
 }));
 
@@ -101,7 +94,7 @@ const CCTable = forwardRef((props, ref) => {
         onScrollEnd && onScrollEnd();
       }
     },
-    [onScrollEnd, loading]
+    [onScrollEnd, loading],
   );
 
   useEffect(() => {
@@ -120,7 +113,7 @@ const CCTable = forwardRef((props, ref) => {
   return (
     <div className={classes.table} ref={ref}>
       <div className={classes.table__column}>
-        {columns.map((column) => (
+        {columns.map(column => (
           <React.Fragment key={column.key}>
             <div
               className={classes.column__content}
@@ -138,11 +131,15 @@ const CCTable = forwardRef((props, ref) => {
         {rows.map((row, index) => (
           <div
             key={index}
-            className={clsx(classes.row__body, {
-              [classes.row__border]: rows.length - 1 !== index,
-              // [classes.row__selected]: selectedRow === index && !disableSelect,
-            })}
-            onClick={(e) => {
+            className={clsx(
+              classes.row__body,
+              {
+                [classes.row__border]: rows.length - 1 !== index,
+                // [classes.row__selected]: selectedRow === index && !disableSelect,
+              },
+              row.className,
+            )}
+            onClick={e => {
               onSelect && onSelect(e, row);
               setSelectedRow(index);
             }}
@@ -173,7 +170,7 @@ CCTable.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   fontSize: PropTypes.number,
-  justify: PropTypes.oneOf(["flex-start", "center", "flex-end"]),
+  justify: PropTypes.oneOf(['flex-start', 'center', 'flex-end']),
   disableSticky: PropTypes.bool,
   disableHover: PropTypes.bool,
   disableSelect: PropTypes.bool,
@@ -183,7 +180,7 @@ CCTable.defaultProps = {
   width: null,
   height: null,
   fontSize: 16,
-  justify: "flex-start",
+  justify: 'flex-start',
   disableSticky: false,
   disableHover: false,
   disableSelect: false,
